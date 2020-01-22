8293919574a5843a7a10a22faa19e9918aa3da69
Darragh Ferry <darraghferry@hotmail.com>
2020-01-22 19:35:12 +0000

    created get all and get by id endpoints

---
+++ schemes/scheme-model.js
@@ -0,0 +1,21 @@
+const db = require("../data/db-config");
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+module.exports = {
+  find,
+  findById
+  //   findSteps,
+  //   add,
+  //   addStep,
+  //   update,
+  //   remove
+};
